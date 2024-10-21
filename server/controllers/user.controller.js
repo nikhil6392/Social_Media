ba17cc1f8a44b0e3ea389d9a246e86c2e5fc9ec1
nikhil6392 <nikhilpathak210431@gmail.com>
2024-10-21 18:35:37 +0530

    Given the structure of crud Operation

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,23 @@
+import User from '../models/user.model'
+import extend from 'lodash/extend'
+import errorHandler from './error.controller.js'
+
+const create =(req,res,next)=>{
+
+}
+
+const list=(req,res)=>{
+
+}
+
+const userByID=(req,res,next,id)=>{}
+
+const read=(req,res)=>{}
+
+const update=(req,res,next)=>{
+
+}
+
+const remove=(req,res,next)=>{}
+
+export default {create,list,userByID,read,update,remove}
